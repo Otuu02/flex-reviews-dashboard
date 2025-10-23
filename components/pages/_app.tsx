46dace454b3eca32b68b1a9a55d07d75d131d7ca
obinna <giftotuuobina1995@gmail.com>
2025-10-23 07:39:38 -0700

    Initial commit - dashboard ready

--- components/pages/_app.tsx
+++ components/pages/_app.tsx
@@ -1,17 +1,11 @@
 // pages/_app.tsx
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Header />
-      <main className="container py-8">
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </>
+    <main className="min-h-screen bg-gray-100">
+      <Component {...pageProps} />
+    </main>
   );
 }
